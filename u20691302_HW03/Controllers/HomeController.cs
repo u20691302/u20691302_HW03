a687b60ae3c8acff671b5dae0b3dfbf15d0d0593
u20691302 <102951728+u20691302@users.noreply.github.com>
2022-06-08 17:18:52 +0200

    Homework Assignment
    First Upload

---
+++ u20691302_HW03/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+
+namespace u20691302_HW03.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        //Declaring variable to storage file path
+        string StorageFilePath = "";
+
+        //Action result ot store files in correct location
+        [HttpPost]
+        public ActionResult Home(FormCollection frm, HttpPostedFileBase Files)
+        {
+            int RadioValue = int.Parse(frm["FileType"]);
+
+            switch (RadioValue)
+            {
+                case 1:
+                    StorageFilePath = "~/App_Data/Files";
+                    break;
+                case 2:
+                    StorageFilePath = "~/App_Data/Images";
+                    break;
+                case 3:
+                    StorageFilePath = "~/App_Data/Videos";
+                    break;
+            }
+
+            if (Files != null && Files.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(Files.FileName);
+                var path = Path.Combine(Server.MapPath(StorageFilePath), fileName);
+                Files.SaveAs(path);
+            }
+            return RedirectToAction("Home");
+        }
+
+
+        public ActionResult Files()
+        {
+            return View();
+        }
+
+        public ActionResult Images()
+        {
+            return View();
+        }
+
+        public ActionResult Videos()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ u20691302_HW03/Models/FileModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace u20691302_HW03.Models
+{
+    public class FileModel
+    {
+        public string Filename { get; set; }
+
+        public HttpPostedFileBase Files { get; set; }
+    }
+}
